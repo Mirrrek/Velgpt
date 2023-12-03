04503df32afa66e81732a8606593bacf74e125e6
Marc Sportiello <mesportiello@gmail.com>
2023-12-03 14:09:14 +0100

    Add detail prop to text components

--- src/components/elements/MediumText.tsx
+++ src/components/elements/MediumText.tsx
@@ -3,12 +3,13 @@ import styles from '@components/elements/MediumText.module.css';
 
 export type MediumTextProps = {
     children?: React.ReactNode;
+    detail?: boolean;
     bold?: boolean;
     centered?: boolean;
 }
 
 export default class MediumText extends React.Component<MediumTextProps> {
     render(): React.ReactNode {
-        return <p className={styles.mediumText + (this.props.bold ? ' ' + styles.bold : '') + (this.props.centered ? ' ' + styles.centered : '')}>{this.props.children}</p>
+        return <p className={styles.mediumText + (this.props.bold ? ' ' + styles.bold : '') + (this.props.centered ? ' ' + styles.centered : '')}>{this.props.children}{this.props.detail ? <span className={styles.detail}> &middot; {this.props.detail}</span> : ''}</p>
     }
 }
--- src/components/elements/SmallText.tsx
+++ src/components/elements/SmallText.tsx
@@ -3,12 +3,13 @@ import styles from '@components/elements/SmallText.module.css';
 
 export type SmallTextProps = {
     children?: React.ReactNode;
+    detail?: string;
     bold?: boolean;
     centered?: boolean;
 }
 
 export default class SmallText extends React.Component<SmallTextProps> {
     render(): React.ReactNode {
-        return <p className={styles.smallText + (this.props.bold ? ' ' + styles.bold : '') + (this.props.centered ? ' ' + styles.centered : '')}>{this.props.children}</p>
+        return <p className={styles.smallText + (this.props.bold ? ' ' + styles.bold : '') + (this.props.centered ? ' ' + styles.centered : '')}>{this.props.children}{this.props.detail ? <span className={styles.detail}> &middot; {this.props.detail}</span> : ''}</p>
     }
 }
