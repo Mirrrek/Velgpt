a89e628c07c963eff7f10b65e48795da3877b86f
Marc Sportiello <mesportiello@gmail.com>
2024-02-25 23:41:26 +0100

    Add spacing option to text container

--- src/components/elements/TextContainer.tsx
+++ src/components/elements/TextContainer.tsx
@@ -3,10 +3,11 @@ import styles from '@components/elements/TextContainer.module.css';
 
 export type TextContainerProps = {
     children?: React.ReactNode;
+    spaced?: boolean;
 }
 
 export default class TextContainer extends React.Component<TextContainerProps> {
     render(): React.ReactNode {
-        return <div className={styles.textContainer}>{this.props.children}</div>
+        return <div className={styles.textContainer + (this.props.spaced ? ' ' + styles.spaced : '')}>{this.props.children}</div>
     }
 }
